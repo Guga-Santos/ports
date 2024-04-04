b1e48bb7ab9706e48ca025823937835be39168ed
Guga-Santos <guga.santos.guga@gmail.com>
2024-04-04 10:46:18 -0300

    fix: z-index at main container

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,47 @@
+'use client'
+import { useEffect, useState } from "react";
 import ports from "../service/ports.mock.json";
 
 export default function Card() {
+    const [index, setIndex] = useState(0);
+    
+    const handleClick = (target: HTMLButtonElement) => {
+        if (target.id === "PREV") {
+            setIndex(index < 1 ? ports.length - 1 : index - 1);
+        } else {
+            setIndex(index >= ports.length - 1 ? 0 : index + 1);
+        }
+    }
+    
+
     return (
-        <div>
-            <h1>
-                {ports[0].port}
+        <div className="border-2 w-96 h-96 flex flex-col items-center justify-center">
+            <h1 className="text-8xl">
+                {ports[index].port[0]}
             </h1>
             <p>
-                {ports[0].description}
+                {ports[index].description}
             </p>
+            <div className="w-44 flex justify-between">
+                <button 
+                type="button"
+                className="z-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                id="PREV"
+                disabled={false}
+                onClick={(event) => {
+                    console.log("PREV button clicked");
+                    handleClick(event.target as HTMLButtonElement);
+                }}>
+                    PREV
+                </button>
+                <button 
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                id="NEXT"
+                onClick={(event) => handleClick(event.target as HTMLButtonElement)}>
+                    NEXT
+                </button>
+            </div>
         </div>
     )
 }
